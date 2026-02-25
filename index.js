370f86ae7ae0d0ca280bc4ac8ef486337b81a42b
Sojib Islam <sojibislam6505@gmail.com>
2026-02-25 10:32:51 +0600

    integrate Stripe payment intent API and store payment data in MongoDB

---
+++ index.js
@@ -0,0 +1,134 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const stripe = require("stripe")(process.env.SECRET_KEY)
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+// MongoDB connection
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@servercluster.nvwzi5y.mongodb.net/?appName=ServerCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db("fastDropDB");
+        const parcelsCollection = db.collection("parcels");
+        const paymentCollection = db.collection("payments");
+        //get parcel API
+        // Get parcels by user email
+        app.get('/parcels', async (req, res) => {
+            const email = req.query.email
+            const query = email ? { senderEmail: email } : {}
+            const result = await parcelsCollection.find(query).toArray()
+            res.send(result)
+        })
+        //get parcel by id
+        app.get("/parcels/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const parcel = await parcelsCollection.findOne({ _id: new ObjectId(id) });
+                if (!parcel) {
+                    return res.status(404).send({ message: "Parcel not found" });
+                }
+                res.send(parcel);
+            } catch (error) {
+                res.status(500).send({ message: "Server error", error });
+            }
+        });
+
+        // Add parcel API
+        app.post("/parcels", async (req, res) => {
+            const parcelData = req.body;
+            const result = await parcelsCollection.insertOne(parcelData);
+            res.send(result)
+        });
+
+        // Mark parcel as paid
+        app.patch("/parcels/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const { transactionId } = req.body;
+
+                // Update parcel in MongoDB
+                const result = await parcelsCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: { paid: true, transactionId: transactionId } }
+                );
+
+                if (result.modifiedCount === 0) {
+                    return res.status(404).send({ message: "Parcel not found or already updated" });
+                }
+
+                res.send({ message: "Payment recorded successfully" });
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ error: error.message });
+            }
+        });
+        //Payment intent
+        app.post("/create-payment-intent", async (req, res) => {
+            try {
+                const { amount } = req.body
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: "usd",
+                    payment_method_types: ["card"]
+                })
+
+                res.send({
+                    clientSecret: paymentIntent.client_secret
+                })
+            } catch (error) {
+                res.status(500).send({ error: error.message })
+            }
+        })
+
+        //Save payment info to database
+        app.post("/payments", async (req, res) => {
+            try {
+                const paymentData = req.body;
+                const result = await paymentCollection.insertOne(paymentData);
+                res.send(result)
+            }
+            catch (error) {
+                console.error(error);
+                res.status(500).send({ error: error.message });
+            }
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
